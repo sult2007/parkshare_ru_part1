590a792bbc3b9e3b70e6799800085f499b614be2
Leis4 <sultgaleev@gmail.com>
2025-12-06 18:31:32 +0300

    asd

--- frontend/next-app/jest.setup.ts
+++ frontend/next-app/jest.setup.ts
@@ -12,3 +12,24 @@ if (!global.crypto) {
   const { webcrypto } = require('crypto');
   global.crypto = webcrypto as Crypto;
 }
+
+if (!global.ReadableStream) {
+  const { ReadableStream } = require('stream/web');
+  global.ReadableStream = ReadableStream as typeof global.ReadableStream;
+}
+
+if (!global.fetch || !global.Response) {
+  const { fetch, Headers, Request, Response } = require('undici');
+  if (!global.fetch) {
+    global.fetch = fetch;
+  }
+  if (!global.Headers) {
+    global.Headers = Headers;
+  }
+  if (!global.Request) {
+    global.Request = Request;
+  }
+  if (!global.Response) {
+    global.Response = Response;
+  }
+}
